121fcd33c5d87ff9aed864cc782365e0ad218a09
Paulo-H-P <fredysoh@gmail.com>
2025-04-03 02:06:58 -0300

    Fix: Corrige caminho dos testes no Cypress

--- cypress-do-zero-a-nuvem/cypress.config.js
+++ cypress-do-zero-a-nuvem/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    specPattern: "cypress-do-zero-a-nuvem/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // Ajuste o caminho aqui!
+    specPattern: "cypress-do-zero-a-nuvem/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "cypress-do-zero-a-nuvem/cypress/support/e2e.js",
   },
 });
