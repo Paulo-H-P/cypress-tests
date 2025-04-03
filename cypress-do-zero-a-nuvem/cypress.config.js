2485ac72bd22c3ee8734651a7238aeaecc9da6ad
Paulo-H-P <fredysoh@gmail.com>
2025-04-03 01:43:45 -0300

    Fix: Update supportFile path in config

--- cypress-do-zero-a-nuvem/cypress.config.js
+++ cypress-do-zero-a-nuvem/cypress.config.js
@@ -1,6 +1,6 @@
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
-    supportFile: false, // Desativa o suporte se não for necessário
+    baseUrl: "http://localhost:3000", // Ou a URL da sua aplicação
+    supportFile: "cypress/support/e2e.js", // Caminho correto do supportFile
   },
 });
